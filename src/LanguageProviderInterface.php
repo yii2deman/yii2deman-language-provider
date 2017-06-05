27775956ccaaee0d3194da4b6b24d1acd8c5e949
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2017-06-05 20:20:37 +0300

    Creates base interface

---
+++ src/LanguageProviderInterface.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * @link https://github.com/yii2deman/yii2deman-language-provider
+ * @copyright Copyright (c) 2017 Vladimir Kuprienko
+ * @license BSD 3-Clause License
+ */
+
+namespace ymaker\email\templates\providers;
+
+/**
+ * Interface for language providers
+ *
+ * @author Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
+ * @since 1.0
+ */
+interface LanguageProviderInterface
+{
+    /**
+     * Method should returns array with languages
+     * Example
+     * ```php
+     * return [
+     *      [
+     *          'label' => 'English',
+     *          'locale' => 'en-US',
+     *      ],
+     *      // ...
+     * ];
+     * ```
+     *
+     * @return array
+     */
+    public function getLanguages();
+
+    /**
+     * Method should returns  array with default language
+     * Example
+     * ```php
+     * return [
+     *      'label' => 'English',
+     *      'locale' => 'en-US',
+     * ];
+     * ```
+     *
+     * @return array
+     */
+    public function getDefaultLanguage();
+}
