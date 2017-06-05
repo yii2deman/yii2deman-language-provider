aea0fdd06de81242dc25ad07c60ee55bf0c32310
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2017-06-05 20:20:53 +0300

    Creates config provider

---
+++ src/ConfigLanguageProvider.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * @link https://github.com/yii2deman/yii2deman-language-provider
+ * @copyright Copyright (c) 2017 Vladimir Kuprienko
+ * @license BSD 3-Clause License
+ */
+
+namespace ymaker\email\templates\providers;
+
+use yii\base\InvalidConfigException;
+use yii\base\Object;
+
+/**
+ * Config language provider
+ *
+ * @author Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
+ * @since 1.0
+ */
+class ConfigLanguageProvider extends Object implements LanguageProviderInterface
+{
+    /**
+     * Contains all languages
+     *
+     * @var array
+     */
+    public $languages = [];
+    /**
+     * Contains one default language
+     *
+     * @var array
+     */
+    public $defaultLanguage = [];
+
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        if (empty($this->languages)) {
+            throw new InvalidConfigException('\'languages\' field cannot be empty');
+        }
+        if (empty($this->defaultLanguage)) {
+            throw new InvalidConfigException('\'defaultLanguage\' field cannot be empty');
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getLanguages()
+    {
+        return $this->languages;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getDefaultLanguage()
+    {
+        return $this->defaultLanguage;
+    }
+}
